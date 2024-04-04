fa4735c77397279919549430aa14e9106933576d
fruzitent <fruzit@gmail.com>
2024-04-04 20:10:22 +0300

    chore: show warning if IPO not supported

--- cmake/InterproceduralOptimization.cmake
+++ cmake/InterproceduralOptimization.cmake
@@ -1,9 +1,10 @@
 macro(myproject_enable_ipo)
   include(CheckIPOSupported)
   check_ipo_supported(RESULT result OUTPUT output)
+
   if(result)
     set(CMAKE_INTERPROCEDURAL_OPTIMIZATION ON)
   else()
-    message(SEND_ERROR "IPO is not supported: ${output}")
+    message(WARNING "IPO is not supported: ${output}")
   endif()
 endmacro()
