edbde06c918cd1ddb64b15b79e9562a47af85120
fruzitent <fruzit@gmail.com>
2024-04-04 21:51:57 +0300

    fix: delay test discovery for cross-builds

--- Dependencies.cmake
+++ Dependencies.cmake
@@ -25,7 +25,7 @@ function(myproject_setup_dependencies)
   endif()
 
   if(NOT TARGET Catch2::Catch2WithMain)
-    cpmaddpackage("gh:catchorg/Catch2@3.3.2")
+    cpmaddpackage("gh:catchorg/Catch2@3.4.0")
   endif()
 
   if(NOT TARGET CLI11::CLI11)
