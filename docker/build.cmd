0a82dd6ed08747b6acb95889e6199f6f620d9461
fruzitent <fruzit@gmail.com>
2024-04-06 16:34:58 +0300

    docs(docker/servercore): add ltsc2025 arm64 reference

--- docker/build.cmd
+++ docker/build.cmd
@@ -1,4 +1,7 @@
 @REM TODO: Docker Desktop v4.29.0 (buildkit v0.13.0)
+
+@REM @see: https://github.com/microsoft/Windows-Containers/issues/389
+@REM @see: https://uupdump.net/download.php?id=6d94591b-25ff-41d9-a6dc-37a152fac397&pack=en-us&edition=serverstandardcore
 @REM TODO: --opt "platform=windows/amd64,windows/arm64"
 
 call :servercore ^
